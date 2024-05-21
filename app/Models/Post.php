3613d59cba397e2f89b31a33968b764e66b16b2f
Koalhack <alexandre.saner@gmail.com>
2024-05-21 13:44:13 +0200

    refactor(backend): create base database structure and models

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'content'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function image()
+    {
+        return $this->belongsTo(Image::class);
+    }
+}
---
+++ database/migrations/2024_05_21_082553_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Image;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('path')->nullable();
+            $table->timestamps();
+        });
+        Schema::table('posts', function (Blueprint $table) {
+            $table->foreignIdFor(Image::class)->nullable()->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('images');
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeignIdFor(Image::class);
+        });
+    }
+};
