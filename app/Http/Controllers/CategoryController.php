25091b8d33d88755b78bbcf31f3b76ddeba8ec69
Koalhack <alexandre.saner@gmail.com>
2024-05-22 10:41:21 +0200

    feat(backend): add Categories API routes (GET, POST, PUT, DELETE)

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends Controller
+{
+    // Category
+    public function getCategories()
+    {
+        return CategoryResource::collection(Category::orderBy('id', 'DESC')->get());
+    }
+
+    public function createCategory(Request $request)
+    {
+        $validators = Validator::make($request->all(), [
+            'name' => 'required|unique:categories|lowercase'
+        ]);
+        if ($validators->passes()) {
+            Category::create([
+                'name' => $request->input('name')
+            ]);
+            return Response::json(['state' => 'success', 'success' => 'Category created successfully']);
+        }
+
+        return Response::json(['state' => 'error', 'error' => $validators->getMessageBag()->toArray()]);
+    }
+
+    public function deleteCategory(string $id)
+    {
+        $category = Category::where('id', $id)->first();
+        if ($category) {
+            $category->delete();
+            return Response::json(['state' => 'success', 'success' => 'Category deleted successfully']);
+        }
+        return Response::json(['state' => 'error', 'error' => 'Category not found!']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
-use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +15,10 @@ Route::controller(PostController::class)->group(function () {
     Route::post("/post/create", 'createPost');
     Route::put("/post/{id}/update", 'updatePost')->where(['id' => '[0-9]+']);
     Route::delete("/post/{id}/delete", 'deletePost')->where(['id' => '[0-9]+']);
+});
+
+Route::controller(CategoryController::class)->group(function () {
+    Route::get("/categories", 'getCategories');
+    Route::post("/category/create", 'createCategory');
+    Route::delete("/category/{id}/delete", 'deleteCategory')->where(['id' => '[0-9]+']);
 });
