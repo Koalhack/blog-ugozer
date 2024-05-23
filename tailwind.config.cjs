1e76242f582fe6e6ab2b68dc26b4dd8edb35afbf
Koalhack <alexandre.saner@gmail.com>
2024-05-23 10:43:15 +0200

    refactor(frontend): set colors, font and container for tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,31 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
+        fontFamily: {
+            figtree: ["Figtree", "sans-serif"],
+        },
+        colors: {
+            transparent: "transparent",
+            carbon: {
+                50: "#FFFFFF",
+                100: "#FFF5EE",
+                200: "#C6C6C6",
+                300: "#181818",
+                900: "#0F0F0F",
+                950: "#000000",
+            },
+            ember: "#DD4E21",
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: "2rem",
+                sm: "2rem",
+                lg: "5rem",
+                xl: "10rem",
+                "2xl": "15rem",
+            },
+        },
         extend: {},
     },
     plugins: [],
