6c4d4303d89dc6cf5a1a8aabe172ac85a88405dc
Koalhack <alexandre.saner@gmail.com>
2024-05-23 13:02:20 +0200

    feat(backend): add timestamp (created_at, update_at) to PostResource API

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -24,6 +24,8 @@ class PostResource extends JsonResource
             'image_path' => $this->image?->path,
             'category_id' => $this->category_id,
             'category_name' => $this->category?->name,
+            'created_at' => $this->created_at,
+            'update_at' => $this->update_at,
         ];
     }
 }
