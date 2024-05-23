a19f9f7e73869d4f53b45ddcf5a4ab9d84655c5d
Koalhack <alexandre.saner@gmail.com>
2024-05-23 13:00:50 +0200

    feat(frontend): add pagination component to index page

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+   activePage: Number,
+   maxPage: Number
+});
+
+const emit = defineEmits(['page']);
+</script>
+
+<template>
+    <div v-if="activePage && maxPage > 1" class="mt-9 mb-3">
+       <ul class="flex justify-center gap-x-8">
+            <li v-for="page in maxPage">
+                <button @click.prevent="emit('page', page)"
+                    class="py-2 px-4 border transition-colors duration-300 ease-in hover:text-carbon-900 hover:bg-carbon-50"
+                    :class="activePage === page ? 'border-carbon-50' : 'border-carbon-900'"
+                >
+                    {{page}}
+                </button>
+            </li>
+        </ul>
+    </div>
+</template>
--- resources/js/Pages/HomeRoute.vue
+++ resources/js/Pages/HomeRoute.vue
@@ -1,5 +1,6 @@
 <script setup>
     import PostCardList from '../Components/PostCardList.vue';
+    import Pagination from '../Components/Pagination.vue';
 
     import axios from 'axios';
     import {ref, watch, onMounted} from 'vue';
@@ -7,25 +8,32 @@
 
     const route = useRoute();
     const response = ref();
+    const activePage = ref(1);
 
-    async function getPost() {
+    async function getPosts() {
         try {
-            response.value = await axios.get(`/api/posts/${route.query.category || ''}`);
+            response.value = await axios.get(`/api/posts/${route.query.category || ''}?page=${activePage.value}`);
         } catch (error) {
             console.log("error post not found: ", error);
         }
     }
 
-    getPost();
-    watch(() => route.query.category, getPost);
+    getPosts();
+    watch(() => route.query.category, () => {
+        getPosts();
+        activePage.value = 1;
+    });
+    watch(activePage, getPosts);
 
 </script>
 
 <template>
     <main class="container pt-[150px]">
-        <router-link to="/test" >Take me to Test page</router-link>
         <section>
             <PostCardList :list="response?.data?.data" />
         </section>
     </main>
+    <footer>
+        <Pagination @page="(page) => activePage = page" :activePage="response?.data?.meta?.current_page" :maxPage="response?.data?.meta?.last_page" />
+    </footer>
 </template>
