d94678637786398d321cc57c1f2f14c5456b3726
Koalhack <alexandre.saner@gmail.com>
2024-05-24 12:06:49 +0200

    fix(frontend): change v-if condition for correct display of Msg

--- resources/js/Components/Msg.vue
+++ resources/js/Components/Msg.vue
@@ -5,8 +5,8 @@
 </script>
 
 <template>
-    <div v-if="response" class="flex justify-between mt-9 px-4 py-3 rounded-sm border border-carbon-300 bg-carbon-900">
-        <span>{{response.data.state}}</span>
+    <div v-if="response?.data" class="flex justify-between mt-9 px-4 py-3 rounded-sm border border-carbon-300 bg-carbon-900">
+        <span>{{response.data.state || ''}}</span>
         <p class="text-carbon-200">{{response.data.success || response.data.error || ''}}</p>
     </div>
 </template>
