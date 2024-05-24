0f2c1474be63d827a38c3c6894f0e044e88db64a
Koalhack <alexandre.saner@gmail.com>
2024-05-24 11:24:23 +0200

    refactor(frontend): remove TestRoute.vue

--- resources/js/router.js
+++ resources/js/router.js
@@ -69,10 +69,6 @@ const router = createRouter({
                 title: "Admin Post",
             },
         },
-        {
-            path: "/test",
-            component: () => import("./Pages/TestRoute.vue"),
-        },
     ],
 });
 
